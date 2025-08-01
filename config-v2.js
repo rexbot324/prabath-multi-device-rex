ad84086ae84e7630721434aac70e267b582e91ed
rexbot324 <rexbot2025@gmail.com>
2025-08-01 18:17:25 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "+94784604369" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~I0bNLkjf3PpB5Xk" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
